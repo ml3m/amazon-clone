646ee4e0e33b8c8e2f3035a7390f15ba86a91cc0
Vlad Ifju <159339398+ml3m@users.noreply.github.com>
2024-07-20 21:53:01 +0200

    removed useless test function that were from imported remote modules'

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,7 @@ import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import {deliveryOptions} from "../data/deliveryOptions.js";
 
-hello();
+
 
 let cartItemsHTML = "";
 
