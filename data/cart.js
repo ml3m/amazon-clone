65d65ea7dab901fdd11b82674bf68b9f0bbc540d
Vlad Ifju <159339398+ml3m@users.noreply.github.com>
2024-07-20 02:37:32 +0200

    loading the cart from localStorage and added default for cart

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,21 @@
-export const cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-},{
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-}];
+export const cart = JSON.parse(localStorage.getItem("cart"));
 
-export function cartSelectorFormat(quantitySelector){
+if (!cart) {
+    cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+    },{
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+    }];
+}
+
+
+function saveToStorage() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function cartSelectorFormat(quantitySelector){
     return parseInt(quantitySelector.value);
 
 }
@@ -30,7 +39,8 @@ export function addToCart(productId, quantitySelector) {
             quantity: selectedValue 
         })
     }
-    
+
+    saveToStorage();
 }
 
 export function addedToCartMessageTimeout(messageAddedToCart){
@@ -66,4 +76,6 @@ export function removeFromCart(productId) {
             cart.splice(index, 1);
         }
     });
+
+    saveToStorage();
 }
